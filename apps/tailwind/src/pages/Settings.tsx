fd40336f36c06ecf9b3c5eac7169415d7bcb12bd
jman <flute999@gmail.com>
2025-06-24 22:38:36 +0900

    Add lucide-react --- to be applied in design

--- apps/tailwind/src/pages/Settings.tsx
+++ apps/tailwind/src/pages/Settings.tsx
@@ -1,7 +1,26 @@
+import { CheckCircle, Hourglass, ClipboardList } from "lucide-react";
+
 export default function Settings() {
   return (
-    <div className="flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Settings</h1>
+    <div>
+      <div className="flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">Settings</h1>
+      </div>
+      <br/>
+      <div className="flex gap-4 items-center">
+        <span className="flex items-center gap-1 text-2xl">
+          <CheckCircle className="text-green-500 w-5 h-5" />
+          Done
+        </span>
+        <span className="flex items-center gap-1 text-2xl">
+          <Hourglass className="text-yellow-500 w-6 h-6" />
+          In Progress
+        </span>
+        <span className="flex items-center gap-1 text-2xl">
+          <ClipboardList className="text-blue-500 w-7 h-7" />
+          To Do
+        </span>
+      </div>
     </div>
   );
 }
